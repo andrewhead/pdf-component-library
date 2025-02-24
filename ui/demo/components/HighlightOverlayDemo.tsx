917f7e30e2df6fa5de1c8e8f765a771b461c37e8
Andrew Head <head@seas.upenn.edu>
2025-02-24 15:11:45 -0500

    Fixed error in repeated keys for rendering

--- ui/demo/components/HighlightOverlayDemo.tsx
+++ ui/demo/components/HighlightOverlayDemo.tsx
@@ -26,8 +26,8 @@ export const HighlightOverlayDemo: React.FunctionComponent<Props> = ({ pageIndex
   function renderTextHighlights(): Array<React.ReactElement> {
     const boxElements: Array<React.ReactElement> = [];
     passages.forEach((passage, pi) => {
+      let renderedAnything = false;
       passage.boxes.map((box, i) => {
-        let renderedAnything = false;
         // Only render this BoundingBox if it belongs on the current page
         if (box.page === pageIndex) {
           const props = {
@@ -40,16 +40,16 @@ export const HighlightOverlayDemo: React.FunctionComponent<Props> = ({ pageIndex
           boxElements.push(<BoundingBox {...props} />);
           renderedAnything = true;
         }
-        if (renderedAnything && passage.boxes.length > 0) {
-          let props: any = { ...passage.boxes[0] };
-          props.top = props.top - 150;  // Offset so we will scroll to this even when the 
-          props.id = `passage-${pi}-scroll-target`;
-          props.className = 'reader__passage-scroll-target'
-          props.key = props.id;
-          props["data-text"] = passage.text;
-          boxElements.push(<BoundingBox {...props} />)
-        }
       });
+      if (renderedAnything && passage.boxes.length > 0) {
+        let props: any = { ...passage.boxes[0] };
+        props.top = props.top - 150;  // Offset so we will scroll to this even when the 
+        props.id = `passage-${pi}-scroll-target`;
+        props.className = 'reader__passage-scroll-target'
+        props.key = props.id;
+        props["data-text"] = passage.text;
+        boxElements.push(<BoundingBox {...props} />)
+      }
     });
     return boxElements;
   }
